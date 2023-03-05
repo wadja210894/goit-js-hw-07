b7664736c319690bde7eb0552ce7cd7bb3db9a5a
Vadym_Hermaniuk <54590021+wadja210894@users.noreply.github.com>
2023-03-05 18:37:04 +0200

    add solution 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const items = galleryItems.map(({original, preview, description})  => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" >
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`}).join("");
+
+const galleryBox = document.querySelector('.gallery');
+galleryBox.innerHTML = items;
+
+galleryBox.addEventListener('click', onLinkClickHandler);
+
+function onLinkClickHandler(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+		<img width="1400" height="900" src="${event.target.dataset.source}">
+	`, {
+        onShow: () => { window.addEventListener('keydown', onEscPress) },
+        onClose: () => { window.removeEventListener('keydown', onEscPress) }
+    });
+    instance.show();
+    
+    function onEscPress(event) {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    };
+}
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const items = galleryItems.map(({original, preview, description}) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`}).join("");
+
+const galleryBox = document.querySelector('.gallery');
+galleryBox.innerHTML = items;
+
+
+const lightbox = new SimpleLightbox('.gallery a', { caption: true, captionSelector: 'img[alt]', captionType: 'attr', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 }); 
+
 console.log(galleryItems);
